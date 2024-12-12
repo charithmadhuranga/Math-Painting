2e18ac4d6f7756b4425e694900ed37c0e817f17c
nikhuge <nikhuge@gmail.com>
2024-12-13 00:32:56 +0530

    add class template

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from symtable import Class
 
 
+class Sqaure:
+    def __init__(self,side,color,x,y):
+        self.side = side
+        self.color = color
+        self.x = x
+        self.y = y
+    def draw(self,canvas):
+        pass
 
+class Rectangle:
+    def __init__(self,width,height,color,x,y):
+        self.width = width
+        self.height = height
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def draw(self,canvas):
+        pass
+
+class Canvas:
+    def __init__(self,width,height,color):
+        self.width = width
+        self.height = height
+        self.color = color
+    def make(self,imagepath):
+        pass
 
 
